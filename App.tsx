c7eb68289ab23ad0a440d42acb0756f9d38aa6ba
caiobga99 <caiobga99@gmail.com>
2023-04-29 21:52:30 -0300

    add a redux setup.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import NavigationBar from "./src/components/NavigationBar/index";
-
+import store from './src/redux/store'
+import { Provider } from 'react-redux'
 export default function App() {
-  return <NavigationBar />;
+  return (
+    <Provider store={store}><NavigationBar /></Provider>
+  ) ;
 }
