7aaf2f5cae83d499c733128a4a10219a47550ee7
caiobga99 <caiobga99@gmail.com>
2023-04-29 21:52:02 -0300

    adding props for especific page.

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,13 +1,13 @@
 import { ActivityIndicator, View } from "react-native";
 
-export default function Loading() {
+export default function Loading({ page }: string | any) {
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#efefef",
+        backgroundColor: page === "Product" ? "#fff" : "#efefef",
       }}
     >
       <ActivityIndicator color="#49a1b8" />
