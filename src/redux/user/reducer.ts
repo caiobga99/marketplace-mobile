a604e6857a0735f15e722c595d3e16442d5a158d
caiobga99 <caiobga99@gmail.com>
2023-04-30 09:25:12 -0300

    adding a user redux.

---
+++ src/redux/user/reducer.ts
@@ -0,0 +1,19 @@
+import UserActionTypes from "./action-types";
+
+const initialState = {
+  currentUser: null,
+};
+
+const userReducer = (state = initialState, action: any) => {
+
+  switch (action.type) {
+    case UserActionTypes.LOGIN:
+      return { ...state, currentUser: action.payload };
+    case UserActionTypes.LOGOUT:
+      return { ...state, currentUser: null };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
