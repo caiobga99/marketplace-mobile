253503410ec44aaf8d73684d51d69269aa217aff
caiobga99 <caiobga99@gmail.com>
2023-05-01 12:43:58 -0300

    adding background props based on the page you are on.

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,13 +1,13 @@
 import { ActivityIndicator, View } from "react-native";
 
-export default function Loading({ page }: string | any) {
+export default function Loading({ background }: string | any) {
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: page === "Product" ? "#fff" : "#efefef",
+        backgroundColor: background || "#efefef",
       }}
     >
       <ActivityIndicator color="#49a1b8" />
