2710c8c41278609574ce4ea6eebb7134969d38a7
caiobga99 <caiobga99@gmail.com>
2023-05-01 12:46:00 -0300

    updating button component.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,34 @@
 import { StyleSheet, TouchableOpacity, Text } from "react-native";
 
+import {
+  useFonts,
+  Inter_900Black,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import Loading from "../Loading";
+
 interface ButtonProps {
   content: string;
   onPress(): void;
 }
+
 const Button = ({ content, onPress }: ButtonProps) => {
+  const [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded)
+    return (
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+        activeOpacity={0.7}
+      >
+        <Loading background="#61dbfb" />
+      </TouchableOpacity>
+    );
+
   return (
     <TouchableOpacity
       style={styles.button}
@@ -26,8 +50,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#61dbfb",
   },
   buttonContent: {
-    fontSize: 15.5,
-    fontWeight: "bold",
+    fontFamily: "Inter_900Black",
+    fontSize: 17,
   },
 });
 
